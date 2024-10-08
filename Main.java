da87d74e839db4e04ed914173c6047cddecb52cb
mafê <113383800+commbeach@users.noreply.github.com>
2024-10-07 21:11:45 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,38 @@
-public class Main{
-  public static void main(String[] args) {
-    //csv file to read
-    String csvFile = "csvDemo.csv";
-    CSVReader.read(csvFile);
-    
-  }
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String csvFile = "csvDemo.csv";
+        Matriz labirinto = CSVReader.read(csvFile);
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a linha da entrada: ");
+        int linhaEntrada = sc.nextInt();
+        System.out.println("Digite a coluna da entrada: ");
+        int colunaEntrada = sc.nextInt();
+
+        System.out.println("Digite a linha da saída: ");
+        int linhaSaida = sc.nextInt();
+        System.out.println("Digite a coluna da saída: ");
+        int colunaSaida = sc.nextInt();
+
+        
+        ArrayList<Integer> caminho_linha = new ArrayList<Integer>();
+        ArrayList<Integer> caminho_coluna = new ArrayList<Integer>();
+
+        boolean caminhoEncontrado = labirinto.encontrarCaminho(linhaEntrada, colunaEntrada, linhaSaida, colunaSaida,caminho_linha, caminho_coluna);
+
+        if (caminhoEncontrado) {
+            System.out.println("Caminho encontrado:");
+            labirinto.print();
+        } else {
+            System.out.println("Nenhum caminho foi encontrado.");
+        }
+
+        sc.close();
+    }
 }
--- Matriz.java
+++ Matriz.java
@@ -1,19 +1,98 @@
-import java.util.ArrayList; 
+import java.util.ArrayList;
 
 public class Matriz {
-   public ArrayList<ArrayList<String>> matriz;
+    public ArrayList<ArrayList<String>> matriz;
 
-   public void print(){
-        for(int i=0;i<this.matriz.size();i++){
+
+    public void print() {
+        for (int i = 0; i < this.matriz.size(); i++) {
             System.out.println(this.matriz.get(i));
         }
-   }
+        System.out.println();
+    }
 
-   public String getelem(int linha,int coluna){
-    ArrayList<String> l = this.matriz.get(linha);
-    String elem = l.get(coluna);
-    return elem;
-   }
 
-   
+    public String getElem(int linha, int coluna) {
+        ArrayList<String> l = this.matriz.get(linha);
+        return l.get(coluna);
+    }
+
+
+    public void setElem(int linha, int coluna, String valor) {
+        this.matriz.get(linha).set(coluna, valor);
+    }
+
+
+    public boolean isValid(int linha, int coluna) {
+        if (linha < 0 || coluna < 0 || linha >= matriz.size() || coluna >= matriz.get(0).size()) {
+            return false;
+        }
+        return getElem(linha, coluna).equals("1");
+    }
+
+
+    private void pause() {
+        try {
+            Thread.sleep(500);  // 0.5 segundo de pausa
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public boolean encontrarCaminho(int linhaAtual, int colunaAtual, int linhaSaida, int colunaSaida, ArrayList<Integer> caminho_linha, ArrayList<Integer> caminho_coluna) {
+
+        System.out.println(caminho_linha);
+        System.out.println(caminho_coluna);
+        System.out.println(linhaAtual);
+        System.out.println(colunaAtual);
+
+        for(int i=0;i<caminho_coluna.size();i++){
+            if(caminho_linha.get(i)==linhaAtual && caminho_coluna.get(i)==colunaAtual){
+                System.out.println(colunaAtual+linhaAtual);
+                setElem(caminho_linha.get(i-1),caminho_coluna.get(i-1),"1");
+                break;
+            }
+        }
+
+
+        if (linhaAtual == linhaSaida && colunaAtual == colunaSaida) {
+            setElem(linhaAtual, colunaAtual, "X");
+            print();
+            return true;
+        }
+
+
+        if (!isValid(linhaAtual, colunaAtual)) {
+            return false;
+        }
+
+
+        setElem(linhaAtual, colunaAtual, "X");
+        caminho_linha.add(linhaAtual);
+        caminho_coluna.add(colunaAtual);
+        print();
+        pause();
+
+
+        int[] deltaLinha = {-1, 1, 0, 0, -1, -1, 1, 1};
+        int[] deltaColuna = {0, 0, -1, 1, -1, 1, -1, 1};
+
+        for (int i = 0; i < 8; i++) {
+            int novaLinha = linhaAtual + deltaLinha[i];
+            int novaColuna = colunaAtual + deltaColuna[i];
+
+
+            if (encontrarCaminho(novaLinha, novaColuna, linhaSaida, colunaSaida, caminho_linha, caminho_coluna)) {
+                return true;
+            }
+        }
+
+
+        setElem(linhaAtual, colunaAtual, "1");
+        print();
+        pause();
+
+        return false;
+    }
 }
